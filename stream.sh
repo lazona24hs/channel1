bd05e0d20e1851d8bf891d6d47917cd5db88c8fc
lazona24hs <jonacaceresok@gmail.com>
2026-09-17 20:18:26 -0300

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # URL del flujo HLS (.m3u8) que quieres retransmitir
-M3U8_URL="http://181.233.180.4:8000/play/a02n/index.m3u8"
+M3U8_URL="http://200.59.213.66:8000/play/a01o/index.m3u8"
 
 echo "Iniciando retransmisión desde fuente M3U8..."
 
